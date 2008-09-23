5ff09156b67e802097cd00af3b38c44afd4e756d
John Resig <jeresig@gmail.com>
2008-09-23 08:55:29 -0700

    Changed the init file to work on load (so that image dependencies are loaded properly).

--- examples/init.js
+++ examples/init.js
@@ -5,8 +5,8 @@
  * the Processing.js engine.
  */
 
-if ( document.addEventListener ) {
-	document.addEventListener("DOMContentLoaded", function() {
+if ( window.addEventListener ) {
+	window.addEventListener("load", function() {
 		var scripts = document.getElementsByTagName("script");
 		
 		for ( var i = 0; i < scripts.length; i++ ) {
