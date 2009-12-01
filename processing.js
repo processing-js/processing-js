1b0c9ba76b00c2101384211cef5690fae16cfd23
f1lt3r <f1lt3r@f1lt3r-desktop.(none)>
2009-12-01 11:52:13 -0500

    Adding splitTokens().

--- processing.js
+++ processing.js
@@ -404,6 +404,42 @@
     // Array handling
     ////////////////////////////////////////////////////////////////////////////    
     
+    p.splitTokens = function( str, tokens ){
+
+      if( arguments.length == 1 ){
+        tokens = "\n\t\r\f ";
+      }
+
+      tokens = "[" + tokens + "]";
+
+      var ary = new Array();
+      var index = 0;
+      var pos = str.search( tokens );
+
+      while( pos >= 0 ){
+        if (pos == 0){
+           str = str.substring( 1 );
+        }else{
+          ary[ index ] = str.substring( 0, pos );
+          index++;
+          str = str.substring( pos );
+        }
+        pos = str.search( tokens );
+      }
+
+      if( str.length > 0 ){
+         ary[ index ] = str;
+      }
+
+      if( ary.length == 0 ){
+          ary = undefined;
+      }
+
+      return ary;
+    }
+
+  
+    
     p.append = function( array, element ){
       array[ array.length ] = element;
       return array;
