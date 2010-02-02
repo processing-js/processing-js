8be80fb5694f813c89ec13fdb8c25b8cc89ee630
mlam <mlam@mlamprimary>
2010-02-01 20:35:23 -0500

    "#191 The text() function accepts only strings" merged with "#97 Cross-browser text() function".

--- processing.js
+++ processing.js
@@ -3172,27 +3172,38 @@
         };
 
         // Print some text to the Canvas
-        p.text = function text(val, x, y) {
+        p.text = function text(str, x, y) {
 
-            if ( typeof val === 'number' && (val+"").indexOf('.') >= 0 ) {
-                
+
+            if ( typeof str === 'number' && (str+"").indexOf('.') >= 0 ) {
+
                 // Make sure .15 rounds to .1, but .151 rounds to .2.
-                if ( (val*1000) - Math.floor( val * 1000 ) == 0.5 ) {
-                  val = val - 0.0001;
+                if ( (str*1000) - Math.floor( str * 1000 ) == 0.5 ) {
+                    str = str - 0.0001;
                 }
-                
-                val = val.toFixed(3);
+
+                str = str.toFixed(3);
             }
-          
             // If the font is a standard Canvas font...
             if (!curTextFont.glyph) {
 
-                if (val && curContext.mozDrawText) {
+                if (str && (curContext.fillText || curContext.mozDrawText)) {
+
 
                     curContext.save();
-                    curContext.mozTextStyle = curTextSize + "px " + curTextFont.name;
-                    curContext.translate(x, y);
-                    curContext.mozDrawText( val ) ;
+                    curContext.font = curContext.mozTextStyle = curTextSize + "px " + curTextFont.name;
+
+                    if (curContext.fillText) {
+
+                        curContext.fillText(str, x, y);
+
+                    } else if (curContext.mozDrawText) {
+
+                        curContext.translate(x, y);
+                        curContext.mozDrawText(str);
+
+                    }
+
                     curContext.restore();
 
                 }
@@ -3205,16 +3216,16 @@
                 curContext.translate(x, y + curTextSize);
 
                 var upem = font.units_per_em,
-                    newScale = 1 / upem * curTextSize;
+                newScale = 1 / upem * curTextSize;
 
                 curContext.scale(newScale, newScale);
 
-                var len = val.length;
+                var len = str.length;
 
                 for (var i = 0; i < len; i++) {
                     // Test character against glyph table
                     try {
-                        p.glyphLook(font, val[i]).draw();
+                        p.glyphLook(font, str[i]).draw();
                     }
                     catch(e) {
                         Processing.debug(e);
