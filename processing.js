3d850db4f57a91d6dec598abcf490581b6fee926
F1LT3R <f1lt3r@aqua.(none)>
2010-02-07 23:11:00 -0500

    #286 Fixed broken right arrow key.

--- processing.js
+++ processing.js
@@ -3510,7 +3510,7 @@
               p.keyCode = p.UP;
               break;
             case 71:
-              keyCode = p.RIGHT;
+              p.keyCode = p.RIGHT;
               break;
             case 72:
               p.keyCode = p.DOWN;
