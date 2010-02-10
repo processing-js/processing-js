fe6c3b3753fd10f07c8b351f82d21d93e92e4c84
Corban Brook <corbanbrook@gmail.com>
2010-02-10 13:49:39 -0500

    #91 curve() added.

--- processing.js
+++ processing.js
@@ -3163,6 +3163,15 @@
 
     };
 
+    p.curve = function curve(x1, y1, x2, y2, x3, y3, x4, y4) {
+      p.beginShape();
+        p.curveVertex(x1, y1);
+        p.curveVertex(x2, y2);
+        p.curveVertex(x3, y3);
+        p.curveVertex(x4, y4);
+      p.endShape();
+    };
+
     p.curveTightness = function (tightness) {
       curTightness = tightness;
     };
@@ -3224,7 +3233,7 @@
       return (1 - t) * (1 - t) * (1 - t) * a + 3 * (1 - t) * (1 - t) * t * b + 3 * (1 - t) * t * t * c + t * t * t * d;
     };
 
-	p.bezierTangent = function bezierTangent(a, b, c, d, t) {
+    p.bezierTangent = function bezierTangent(a, b, c, d, t) {
       return ( 3 * t * t * ( -a + 3 * b -3 * c + d ) +6 *t * ( a - 2 * b + c ) + 3 * ( -a + b ) );
     };
 	
