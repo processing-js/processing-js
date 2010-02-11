074e04e007c884d7d237409691ecff198e7ea81d
Corban Brook <corbanbrook@gmail.com>
2010-02-10 22:23:44 -0500

    Minor formatting issues with p.frustum

--- processing.js
+++ processing.js
@@ -2866,14 +2866,14 @@
 			}
 		};
 
-		p.frustum = function frustum( left, right, bottom, top, near, far ){
-			frustumMode = true;
-			projection = new PMatrix3D();
-			projection.set( (2*near)/(right-left), 0 , (right+left)/(right-left),	0 ,
-							0, (2*near)/(top-bottom), (top+bottom)/(top-bottom), 0 ,
-							0, 0 , -(far+near)/(far-near), -(2*far*near)/(far-near) ,
-							0, 0 , -1, 0 );
-		};
+    p.frustum = function frustum( left, right, bottom, top, near, far ) {
+      frustumMode = true;
+      projection = new PMatrix3D();
+      projection.set( (2*near)/(right-left), 0 , (right+left)/(right-left),	0 ,
+        0, (2*near)/(top-bottom), (top+bottom)/(top-bottom), 0 ,
+        0, 0 , -(far+near)/(far-near), -(2*far*near)/(far-near) ,
+        0, 0 , -1, 0 );
+    };
 
     p.ortho = function ortho() {
       if ( arguments.length === 0 ) {
