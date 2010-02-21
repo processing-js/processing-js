e27f5b1c6a6d5fe6107fba576eb5d362056deaf2
Andor <andor@c-leungs-macbook-pro.local>
2010-02-21 17:08:12 -0500

    implemented rotate and added tests.

--- processing.js
+++ processing.js
@@ -2957,6 +2957,23 @@
           this.elements = result.slice();
         }
       },
+      rotate: function( angle, v0, v1, v2 ) {
+        if( !v1 ) {
+          this.rotateZ( angle );
+        }
+        else {
+          // TODO should make sure this vector is normalized
+
+          var c = p.cos( angle );
+          var s = p.sin( angle );
+          var t = 1.0 - c;
+
+          this.apply( (t*v0*v0) + c, (t*v0*v1) - (s*v2), (t*v0*v2) + (s*v1), 0,
+                      (t*v0*v1) + (s*v2), (t*v1*v1) + c, (t*v1*v2) - (s*v0), 0,
+                      (t*v0*v2) - (s*v1), (t*v1*v2) + (s*v0), (t*v2*v2) + c, 0,
+                      0, 0, 0, 1);
+        }
+      },
       rotateX: function( angle ){
         var c = p.cos( angle );
         var s = p.sin( angle );
