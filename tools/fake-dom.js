b042bf545fffab65c197f2fcd3b00d46140dfc63
Corban Brook <corbanbrook@gmail.com>
2010-02-22 22:15:20 -0500

    #346 Fix fake-dom to work with println and tinylog. (humphd)

--- tools/fake-dom.js
+++ tools/fake-dom.js
@@ -1,9 +1,14 @@
 // Processing works with a canvas and the DOM, fake it.
+// This is enough of the DOM to allow the parser work.
+
 var __empty_func__ = function () {};
+var __elem_func__ = function() { return elem };
 
 var canvas = {
   attachEvent: __empty_func__,
   addEventListener: __empty_func__,
+  appendChild: __elem_func__,
+  removeChild: __empty_func__,
   getContext:  function() {
                  return {
                    translate: __empty_func__,
@@ -34,28 +39,46 @@ var canvas = {
                    createProgram: __empty_func__,
                    attachShader: __empty_func__,
                    linkProgram: __empty_func__,
-                   getProgramParameter: function() { return true; }, 
+                   getProgramParameter: function() { return true; },
                    useProgram: __empty_func__,
                    createBuffer: __empty_func__,
                    bindBuffer: __empty_func__,
                    bufferData: __empty_func__
                  };
-  }
+  },
+  style: {}
 };
 
 var WebGLFloatArray = __empty_func__;
 
-// This is enough of the DOM to allow the parser work.
 var document = {
+  fake: true,
   attachEvent: __empty_func__,
   body: {
     style: {
       cursor: {}
     }
   },
+  appendChild: __elem_func__,
+  removeChild: __empty_func__,
   getElementByTagName: function() { return canvas; },
   createElement: function () { return canvas },
-  addEventListener: __empty_func__
+  addEventListener: __empty_func__,
+  documentElement: {
+    appendChild: __elem_func__,
+    removeChild: __empty_func__,
+    style: {
+      paddingBottom: 0
+    },
+    insertBefore: __empty_func__
+  },
+  createTextNode: __empty_func__
+};
+
+var elem = {
+  appendChild: __elem_func__,
+  removeChild: __empty_func__,
+  style: {}
 };
 
 var addEventListener = __empty_func__;
@@ -64,8 +87,13 @@ var setInterval = __empty_func__;
 var clearInterval = __empty_func__;
 
 var window = {
+  appendChild: __elem_func__,
+  removeChild: __empty_func__,
   setInterval: __empty_func__,
-  XMLHttpRequest: __empty_func__
+  XMLHttpRequest: __empty_func__,
+  print: print,
+  document: document,
+  createElement: function() { return elem; }
 };
 
 // Constructors not included in many parser tests to allow them to run
