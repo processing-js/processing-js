467fe27dfc382a07a9f8a5e280ba41b3fd55596b
mlam <mlam@mlammobile>
2010-03-23 22:57:34 -0400

    Need another newline.

--- processing.js
+++ processing.js
@@ -3511,7 +3511,7 @@
 
         output += p.nfs(this.elements[3], digits, 4) + " " +
           p.nfs(this.elements[4], digits, 4) + " " +
-          p.nfs(this.elements[5], digits, 4) + "\n";
+          p.nfs(this.elements[5], digits, 4) + "\n\n";
         
         p.println(output);
       }
@@ -3921,7 +3921,7 @@
         output += p.nfs(this.elements[12], digits, 4) + " " +
           p.nfs(this.elements[13], digits, 4) + " " +
           p.nfs(this.elements[14], digits, 4) + " " +
-          p.nfs(this.elements[15], digits, 4) + "\n";
+          p.nfs(this.elements[15], digits, 4) + "\n\n";
 
         p.println(output);
       }
