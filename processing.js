6a45a98443f7f99f67eddb0c20ba5f09fce2dbe8
Thanh Dao <tdao75@yahoo.ca>
2010-04-15 00:05:26 -0400

    createInput using ajax

--- processing.js
+++ processing.js
@@ -2986,7 +2986,16 @@
     p.enableContextMenu = function enableContextMenu() {
       curElement.removeEventListener('contextmenu', contextMenu, false);
     };
-
+     
+	p.function createInput(filename) {  
+      if (window.XMLHttpRequest){
+        var fileRequest = new XMLHttpRequest();  
+        fileRequest.open('GET', filename, false);  
+        fileRequest.send(null);  
+        if (fileRequest.status != 200) return null; 
+        return fileRequest.responseText;  
+      }
+   }; 
 
     ////////////////////////////////////////////////////////////////////////////
     // Binary Functions
