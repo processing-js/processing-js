812f3dbd222764ddb6e202a3cb48c15382b8855b
Andor <andor@c-leungs-macbook-pro-2.local>
2010-06-04 14:39:01 -0400

    fixed clamp problem, updated demo, added noclose test, works

--- processing.js
+++ processing.js
@@ -5572,7 +5572,10 @@
               else{
                 line3D(lineVertArray, "LINE_STRIP", strokeVertArray);
               }
-              if(doFill){//!!!!!
+              
+              // fill is ignored if textures are used
+              if(doFill || usingTexture){
+              //!!!!!
               //p.println(texVertArray.length + " - " + colorVertArray.length);
                 fill3D(fillVertArray, "TRIANGLE_FAN", colorVertArray, texVertArray);
               }
@@ -5756,8 +5759,8 @@
         curContext.bindTexture(curContext.TEXTURE_2D, pimage.__texture);        
         curContext.texParameteri(curContext.TEXTURE_2D, curContext.TEXTURE_MIN_FILTER, curContext.LINEAR);
         curContext.texParameteri(curContext.TEXTURE_2D, curContext.TEXTURE_MAG_FILTER, curContext.LINEAR);
-//        curContext.texParameteri(curContext.TEXTURE_2D, curContext.TEXTURE_WRAP_T, curContext.CLAMP_TO_EDGE);
-  //      curContext.texParameteri(curContext.TEXTURE_2D, curContext.TEXTURE_WRAP_S, curContext.CLAMP_TO_EDGE);
+        curContext.texParameteri(curContext.TEXTURE_2D, curContext.TEXTURE_WRAP_T, curContext.CLAMP_TO_EDGE);
+        curContext.texParameteri(curContext.TEXTURE_2D, curContext.TEXTURE_WRAP_S, curContext.CLAMP_TO_EDGE);
         curContext.texImage2D(curContext.TEXTURE_2D, 0, pimage.__cvs, false);
       }
       else{
