10d132ebdd446034a2789e06a4e6cafc040a8d4f
Corban Brook <corbanbrook@gmail.com>
2010-07-22 10:13:54 -0400

    #816 Move PVector outside of the Processing constructor

--- processing.js
+++ processing.js
@@ -30,9 +30,150 @@
     return xhr.responseText; 
   };
 
+  var PVector = function(x, y, z) {
+    this.x = x || 0;
+    this.y = y || 0;
+    this.z = z || 0;
+  },
+  createPVectorMethod = function(method) {
+    return function(v1, v2) {
+      var v = v1.get();
+      v[method](v2);
+      return v;
+    };
+  },
+  createSimplePVectorMethod = function(method) {
+    return function(v1, v2) {
+      return v1[method](v2);
+    };
+  },
+  simplePVMethods = "dist dot cross".split(" "),
+  method = simplePVMethods.length;
+
+  PVector.angleBetween = function(v1, v2) {
+    return Math.acos(v1.dot(v2) / (v1.mag() * v2.mag()));
+  };
+
+  // Common vector operations for PVector
+  PVector.prototype = {
+    set: function(v, y, z) {
+      if (arguments.length === 1) {
+        this.set(v.x || v[0], v.y || v[1], v.z || v[2]);
+      } else {
+        this.x = v;
+        this.y = y;
+        this.z = z;
+      }
+    },
+    get: function() {
+      return new PVector(this.x, this.y, this.z);
+    },
+    mag: function() {
+      return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+    },
+    add: function(v, y, z) {
+      if (arguments.length === 3) {
+        this.x += v;
+        this.y += y;
+        this.z += z;
+      } else if (arguments.length === 1) {
+        this.x += v.x;
+        this.y += v.y;
+        this.z += v.z;
+      }
+    },
+    sub: function(v, y, z) {
+      if (arguments.length === 3) {
+        this.x -= v;
+        this.y -= y;
+        this.z -= z;
+      } else if (arguments.length === 1) {
+        this.x -= v.x;
+        this.y -= v.y;
+        this.z -= v.z;
+      }
+    },
+    mult: function(v) {
+      if (typeof v === 'number') {
+        this.x *= v;
+        this.y *= v;
+        this.z *= v;
+      } else if (typeof v === 'object') {
+        this.x *= v.x;
+        this.y *= v.y;
+        this.z *= v.z;
+      }
+    },
+    div: function(v) {
+      if (typeof v === 'number') {
+        this.x /= v;
+        this.y /= v;
+        this.z /= v;
+      } else if (typeof v === 'object') {
+        this.x /= v.x;
+        this.y /= v.y;
+        this.z /= v.z;
+      }
+    },
+    dist: function(v) {
+      var dx = this.x - v.x,
+          dy = this.y - v.y,
+          dz = this.z - v.z;
+      return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    },
+    dot: function(v, y, z) {
+      if (arguments.length === 3) {
+        return (this.x * v + this.y * y + this.z * z);
+      } else if (arguments.length === 1) {
+        return (this.x * v.x + this.y * v.y + this.z * v.z);
+      }
+    },
+    cross: function(v) {
+      return new PVector(this.y * v.z - v.y * this.z,
+                         this.z * v.x - v.z * this.x,
+                         this.x * v.y - v.x * this.y);
+    },
+    normalize: function() {
+      var m = this.mag();
+      if (m > 0) {
+        this.div(m);
+      }
+    },
+    limit: function(high) {
+      if (this.mag() > high) {
+        this.normalize();
+        this.mult(high);
+      }
+    },
+    heading2D: function() {
+      return (-Math.atan2(-this.y, this.x));
+    },
+    toString: function() {
+      return "[" + this.x + ", " + this.y + ", " + this.z + "]";
+    },
+    array: function() {
+      return [this.x, this.y, this.z];
+    }
+  };
+
+  while (method--) {
+    PVector[simplePVMethods[method]] = createSimplePVectorMethod(simplePVMethods[method]);
+  }
+
+  for (method in PVector.prototype) {
+    if (PVector.prototype.hasOwnProperty(method) && !PVector.hasOwnProperty(method)) {
+      PVector[method] = createPVectorMethod(method);
+    }
+  }
+
+
   var Processing = this.Processing = function Processing(curElement, aCode) {
 
     var p = this;
+
+    // Include Classes
+    p.PVector = PVector;
+    //p.PShapeSVG = PShapeSVG; 
 
     p.name = 'Processing.js Instance'; // Set Processing defaults / environment variables
     p.use3DContext = false; // default '2d' canvas context
@@ -2588,146 +2729,6 @@
       }
     };
    
-    ////////////////////////////////////////////////////////////////////////////
-    // PVector
-    ////////////////////////////////////////////////////////////////////////////
-    var PVector = p.PVector = function(x, y, z) {
-      this.x = x || 0;
-      this.y = y || 0;
-      this.z = z || 0;
-    },
-    createPVectorMethod = function(method) {
-      return function(v1, v2) {
-        var v = v1.get();
-        v[method](v2);
-        return v;
-      };
-    },
-    createSimplePVectorMethod = function(method) {
-      return function(v1, v2) {
-        return v1[method](v2);
-      };
-    },
-    simplePVMethods = "dist dot cross".split(" "),
-    method = simplePVMethods.length;
-
-    PVector.angleBetween = function(v1, v2) {
-      return Math.acos(v1.dot(v2) / (v1.mag() * v2.mag()));
-    };
-
-    // Common vector operations for PVector
-    PVector.prototype = {
-      set: function(v, y, z) {
-        if (arguments.length === 1) {
-          this.set(v.x || v[0], v.y || v[1], v.z || v[2]);
-        } else {
-          this.x = v;
-          this.y = y;
-          this.z = z;
-        }
-      },
-      get: function() {
-        return new PVector(this.x, this.y, this.z);
-      },
-      mag: function() {
-        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
-      },
-      add: function(v, y, z) {
-        if (arguments.length === 3) {
-          this.x += v;
-          this.y += y;
-          this.z += z;
-        } else if (arguments.length === 1) {
-          this.x += v.x;
-          this.y += v.y;
-          this.z += v.z;
-        }
-      },
-      sub: function(v, y, z) {
-        if (arguments.length === 3) {
-          this.x -= v;
-          this.y -= y;
-          this.z -= z;
-        } else if (arguments.length === 1) {
-          this.x -= v.x;
-          this.y -= v.y;
-          this.z -= v.z;
-        }
-      },
-      mult: function(v) {
-        if (typeof v === 'number') {
-          this.x *= v;
-          this.y *= v;
-          this.z *= v;
-        } else if (typeof v === 'object') {
-          this.x *= v.x;
-          this.y *= v.y;
-          this.z *= v.z;
-        }
-      },
-      div: function(v) {
-        if (typeof v === 'number') {
-          this.x /= v;
-          this.y /= v;
-          this.z /= v;
-        } else if (typeof v === 'object') {
-          this.x /= v.x;
-          this.y /= v.y;
-          this.z /= v.z;
-        }
-      },
-      dist: function(v) {
-        var dx = this.x - v.x,
-            dy = this.y - v.y,
-            dz = this.z - v.z;
-        return Math.sqrt(dx * dx + dy * dy + dz * dz);
-      },
-      dot: function(v, y, z) {
-        if (arguments.length === 3) {
-          return (this.x * v + this.y * y + this.z * z);
-        } else if (arguments.length === 1) {
-          return (this.x * v.x + this.y * v.y + this.z * v.z);
-        }
-      },
-      cross: function(v) {
-        return new PVector(this.y * v.z - v.y * this.z,
-                           this.z * v.x - v.z * this.x,
-                           this.x * v.y - v.x * this.y);
-      },
-      normalize: function() {
-        var m = this.mag();
-        if (m > 0) {
-          this.div(m);
-        }
-      },
-      limit: function(high) {
-        if (this.mag() > high) {
-          this.normalize();
-          this.mult(high);
-        }
-      },
-      heading2D: function() {
-        return (-Math.atan2(-this.y, this.x));
-      },
-      toString: function() {
-        return "[" + this.x + ", " + this.y + ", " + this.z + "]";
-      },
-      array: function() {
-        return [this.x, this.y, this.z];
-      }
-    };
-
-    while (method--) {
-      PVector[simplePVMethods[method]] = createSimplePVectorMethod(simplePVMethods[method]);
-    }
-
-    for (method in PVector.prototype) {
-      if (PVector.prototype.hasOwnProperty(method) && !PVector.hasOwnProperty(method)) {
-        PVector[method] = createPVectorMethod(method);
-      }
-    }
-
-    p.PVector = PVector;
 
     ////////////////////////////////////////////////////////////////////////////
     // 2D Matrix
---
+++ test/unit/PVector.pde
@@ -0,0 +1,4 @@
+PVector v = new PVector(0.0, 0.0, 0.0);
+v.set(20.0, 30.0, 40.0);
+
+_checkEqual([v.x, v.y, v.z], [20.0, 30.0, 40.0]);
