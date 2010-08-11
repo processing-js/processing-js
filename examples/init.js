d46748e899d5f4edd4dadcf36d992d6903aa03f0
Anna Sobiepanek <annaopensource@gmail.com>
2010-08-11 15:55:11 -0400

    cleaned up branch removed irrelevant stuff binary.html works on all browsers branch canvasDiv [#192 state:super-review-requested]

--- examples/init.js
+++ examples/init.js
@@ -5,26 +5,25 @@
  * the Processing.js engine.
  */
 
-if ( window.addEventListener ) {
+if (window.addEventListener) {
   window.addEventListener("load", function() {
     var scripts = document.getElementsByTagName("script");
  
-    for ( var i = 0; i < scripts.length; i++ ) {
-      if ( scripts[i].type == "application/processing" ) {
+    for (var i = 0; i < scripts.length; i++) {
+      if (scripts[i].type == "application/processing") {
         var src = scripts[i].getAttribute("target");
-        var canvas = scripts[i].nextSibling;
- 
-        if ( src && src.indexOf("#") > -1 ) {
-          canvas = document.getElementById( src.substr( src.indexOf("#") + 1 ) );
-        } else {
-          while ( canvas && canvas.nodeName.toUpperCase() != "CANVAS" ) {
-            canvas = canvas.nextSibling;
+        var canvas;
+        if (src && src.indexOf("#") > -1) {
+          canvas = document.getElementById(src.substr(src.indexOf("#") + 1));
+          if (canvas) {
+            new Processing(canvas, scripts[i].text);
           }
-        }
- 
-        if ( canvas ) {
-          new Processing(canvas, scripts[i].text);
-        }
+        } else {
+          canvas =  document.getElementsByTagName("canvas");;
+          for (var j =0; j< canvas.length; j ++) {
+            new Processing(canvas[j], scripts[i].text);
+          }
+        }       
       }
     }
   }, false);
