d402df71464d25db6e1773acb0b4d05482ff9cc8
Anna Sobiepanek <annaopensource@gmail.com>
2010-08-11 23:11:12 -0400

    reset binary example and added init example fixed code as per request [#192 state:peer-review-requested]

--- examples/init.js
+++ examples/init.js
@@ -8,21 +8,25 @@
 if (window.addEventListener) {
   window.addEventListener("load", function() {
     var scripts = document.getElementsByTagName("script");
- 
-    for (var i = 0; i < scripts.length; i++) {
+    var canvasArray =  document.getElementsByTagName("canvas");
+    var canvas;
+    var offset = 0;
+    for (var i = 0, j = 0; i < scripts.length; i++) {
       if (scripts[i].type == "application/processing") {
         var src = scripts[i].getAttribute("target");
-        var canvas;
         if (src && src.indexOf("#") > -1) {
           canvas = document.getElementById(src.substr(src.indexOf("#") + 1));
           if (canvas) {
             new Processing(canvas, scripts[i].text);
+            offset++;
+            j++;
           }
         } else {
-          canvas =  document.getElementsByTagName("canvas");;
-          for (var j =0; j< canvas.length; j ++) {
-            new Processing(canvas[j], scripts[i].text);
+          
+          if (canvasArray.length >= i- offset) {
+            new Processing(canvasArray[j], scripts[i].text);          
           }
+          j++;
         }       
       }
     }
