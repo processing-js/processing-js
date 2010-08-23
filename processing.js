5e40e83db91a066bd0c92be4fdee6e3bf9e9682c
Anna Sobiepanek <annaopensource@gmail.com>
2010-08-23 11:48:57 -0400

    added #00F notation to setFill and setStroke [#801 state:peer-review-requested]

--- processing.js
+++ processing.js
@@ -2951,7 +2951,11 @@
           this.fill = false;
         } else if (fillText.indexOf("#") === 0) {
           this.fill      = true;
-          this.fillColor = opacityMask | (parseInt(fillText.substring(1), 16 )) & 0xFFFFFF;
+          if (fillText.length === 4) {
+            // convert #00F to #0000FF
+            fillText = fillText.replace(/#(.)(.)(.)/,"#$1$1$2$2$3$3");
+          }
+          this.fillColor = opacityMask | (parseInt(fillText.substring(1), 16)) & 0xFFFFFF;
         } else if (fillText.indexOf("rgb") === 0) {
           this.fill      = true;
           this.fillColor = opacityMask | this.parseRGB(fillText);
@@ -2982,8 +2986,12 @@
           this.stroke = false;
         } else if (strokeText.charAt( 0 ) === "#") {
           this.stroke      = true;
-          this.strokeColor = opacityMask | (parseInt( strokeText.substring( 1 ), 16 )) & 0xFFFFFF;
-        } else if (strokeText.indexOf( "rgb" ) === 0 ) {
+          if (strokeText.length === 4) {
+            // convert #00F to #0000FF
+            strokeText = strokeText.replace(/#(.)(.)(.)/,"#$1$1$2$2$3$3");
+          }
+          this.strokeColor = opacityMask | (parseInt( strokeText.substring(1), 16)) & 0xFFFFFF;
+         } else if (strokeText.indexOf( "rgb" ) === 0 ) {
           this.stroke = true;
           this.strokeColor = opacityMask | this.parseRGB(strokeText);
         } else if (strokeText.indexOf( "url(#" ) === 0) {
