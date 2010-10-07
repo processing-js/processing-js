55f665d3ac1ee179f0ede518ed13fd4ecd861f2b
notmasteryet <async.processingjs@yahoo.com>
2010-10-07 18:16:25 -0500

    Abstract methods

--- processing.js
+++ processing.js
@@ -12308,8 +12308,9 @@
     function transformClassMethod(method) {
       var m = methodsRegex.exec(method);
       methodsRegex.lastIndex = 0;
+      var body = m[6] !== ';' ? atoms[getAtomIndex(m[6])] : "{}";
       return new AstClassMethod(m[3], transformParams(atoms[getAtomIndex(m[4])]),
-        transformStatementsBlock(atoms[getAtomIndex(m[6])]) );
+        transformStatementsBlock(body) );
     }
 
     function AstClassField(definitions, fieldType, isStatic) {
