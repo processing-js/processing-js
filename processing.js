1585d140dd5092ea93800bf7d8eb7b745e2d0a59
Daniel Hodgin <dhodgin@gmail.com>
2010-10-08 21:03:17 -0400

    2nd fix by notmasteryet for resizeimage example

--- processing.js
+++ processing.js
@@ -9284,7 +9284,8 @@
           p.endShape();
         } else {
           var bounds = imageModeConvert(x || 0, y || 0, w || img.width, h || img.height, arguments.length < 4);
-          var fastImage = ("sourceImg" in img) && curTint === null && !img.__mask;
+          //var fastImage = ("sourceImg" in img) && curTint === null && !img.__mask;
+          var fastImage = !!img.sourceImg && curTint === null && !img.__mask;
           if (fastImage) {
             var htmlElement = img.sourceImg;
             // Using HTML element's width and height in case if the image was resized.
