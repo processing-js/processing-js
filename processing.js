a57a57821a7dc4a82ccb85251e969a69945aae13
Andor <andor@MBP-55.local>
2011-01-18 19:04:21 -0500

    added fix

--- processing.js
+++ processing.js
@@ -22,6 +22,7 @@
 
   var ajax = function ajax(url) {
     var xhr = new XMLHttpRequest();
+    xhr.overrideMimeType("application/json");
     xhr.open("GET", url, false);
     xhr.setRequestHeader("If-Modified-Since", "Fri, 01 Jan 1960 00:00:00 GMT");
     xhr.send(null);
