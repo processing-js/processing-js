39456b3736de1e5111c547abe3fa6aa201e38dd8
notmasteryet <async.processingjs@yahoo.com>
2011-03-07 17:35:09 -0600

    Fix ensureLoad function to create proper amount of buckets

--- processing.js
+++ processing.js
@@ -678,8 +678,9 @@
             allEntries = allEntries.concat(buckets[i]);
           }
         }
+        var newBucketsLength = buckets.length * 2;
         buckets = [];
-        buckets.length = buckets.length * 2;
+        buckets.length = newBucketsLength;
         for (var j = 0; j < allEntries.length; ++j) {
           var index = getBucketIndex(allEntries[j].key);
           var bucket = buckets[index];
