9e21d052ac914577601177c760cc6fbec84b88ee
Pomax <Pomax@nihongoresources.com>
2011-05-11 20:06:05 -0700

    fixes + unit tests (were in 1209, but essential to this ticket too)

--- processing.js
+++ processing.js
@@ -4290,38 +4290,36 @@
        * @return {boolean} true if the element equals another element
        */
       equals: function(other) {
-        if (other instanceof XMLElement) {
-          var i, j;
-          if (this.name !== other.getLocalName()) { return false; }
-          if (this.attributes.length !== other.getAttributeCount()) { return false; }
-
-          // attributes may be ordered differently
-          if (this.attributes.length !== other.attributes.length) { return false; }
-          var attr_name, attr_ns, attr_value, attr_type, attr_other;
-          for (i = 0, j = this.attributes.length; i < j; i++) {
-            attr_name = this.attributes[i].getName();
-            attr_ns = this.attributes[i].getNamespace();
-            attr_other = other.findAttribute(attr_name, attr_ns);
-            if (attr_other === null) { return false; }
-            if (this.attributes[i].getValue() !== attr_other.getValue()) { return false; }
-            if (this.attributes[i].getType() !== attr_other.getType()) { return false; }
-          }
-
-          // children must be ordered identically
-          if (this.children.length !== other.getChildCount()) { return false; }
-          if (this.children.length>0) {
-            var child1, child2;
-            for (i = 0, j = this.children.length; i < j; i++) {
-              child1 = this.getChild(i);
-              child2 = other.getChild(i);
-              if (!child1.equals(child2)) { return false; }
-            }
-            return true;
-          } else {
-            return (this.content === other.content);
-          }
+        if (!(other instanceof XMLElement)) {
+          return false;
         }
-        return false;
+        var i, j;
+        if (this.name !== other.getLocalName()) { return false; }
+        if (this.attributes.length !== other.getAttributeCount()) { return false; }
+        // attributes may be ordered differently
+        if (this.attributes.length !== other.attributes.length) { return false; }
+        var attr_name, attr_ns, attr_value, attr_type, attr_other;
+        for (i = 0, j = this.attributes.length; i < j; i++) {
+          attr_name = this.attributes[i].getName();
+          attr_ns = this.attributes[i].getNamespace();
+          attr_other = other.findAttribute(attr_name, attr_ns);
+          if (attr_other === null) { return false; }
+          if (this.attributes[i].getValue() !== attr_other.getValue()) { return false; }
+          if (this.attributes[i].getType() !== attr_other.getType()) { return false; }
+        }
+        // children must be ordered identically
+        if (this.children.length !== other.getChildCount()) { return false; }
+        if (this.children.length>0) {
+          var child1, child2;
+          for (i = 0, j = this.children.length; i < j; i++) {
+            child1 = this.getChild(i);
+            child2 = other.getChild(i);
+            if (!child1.equals(child2)) { return false; }
+          }
+          return true;
+        } else {
+          return (this.content === other.content);
+        }
       },
       /**
        * @member XMLElement
@@ -4717,7 +4715,6 @@
       setContent: function(content) {
         if (this.children.length>0) {
           Processing.debug("Tried to set content for XMLElement with children"); }
-        createPCDataElement
         this.content = content;
       },
       /**
