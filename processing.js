fa0467dad05266e498ef558ec47b21f11bff90f2
David Humphrey <david.humphrey@senecac.on.ca>
2011-07-11 15:59:23 -0400

    Improve error message and README info for xhr origin policy load errors.

--- processing.js
+++ processing.js
@@ -19440,7 +19440,7 @@
             if ( ("withCredentials" in new XMLHttpRequest) &&
                  (new XMLHttpRequest).withCredentials === false &&
                  window.location.protocol === "file:" ) {
-              error = "XMLHttpRequest failure, perhaps due to an origin policy violation. Try loading this page in another browser, or load it from http://localhost using a local webserver.";
+              error = "XMLHttpRequest failure, possibly due to a same-origin policy violation. You can try loading this page in another browser, or load it from http://localhost using a local webserver. See the Processing.js README for a more detailed explanation of this problem and solutions.";
             } else {
               error = "File is empty.";
             }
