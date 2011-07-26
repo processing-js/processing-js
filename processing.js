11a250722d22118c0f30b273c7cb21f9a5d76ab3
Pomax <Pomax@nihongoresources.com>
2011-07-25 19:40:34 -0700

    fixed spacing

--- processing.js
+++ processing.js
@@ -8603,7 +8603,7 @@
         return subject.equalsIgnoreCase.apply(subject, removeFirstArgument(arguments));
       }
 
-      return subject.toLowerCase()===other.toLowerCase();
+      return subject.toLowerCase() === other.toLowerCase();
     };
     /**
      * The __toCharArray() function splits the string into a char array.
