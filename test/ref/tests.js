7094194b8f4254a5bb45bd0f3a5caebb77a1c165
Pomax <Pomax@nihongoresources.com>
2011-07-25 21:28:04 -0700

    updated epsilons, regenerated baseline test

--- test/ref/tests.js
+++ test/ref/tests.js
@@ -84,14 +84,14 @@ var tests = [
   { path: "text-width_comic.pde", tags:["2D", "Text"], epsilonOverride: 0.35 }, /* See ticket #1172 */
   { path: "text-leading.pde", tags: ["2D", "Text"], epsilonOverride: 0.20 }, /* See ticket #1172 */
   { path: "text-font-fromfile.pde", tags: ["2D", "Text"], epsilonOverride: 0.22 }, /* See ticket #1172 */
-  { path: "text-valign-BOTTOM.pde", tags: ["2D", "Text"] },
-  { path: "text-valign-BASELINE.pde", tags: ["2D", "Text"], epsilonOverride: 0.09 }, /* See ticket #1172 */
-  { path: "text-valign-CENTER.pde", tags: ["2D", "Text"], epsilonOverride: 0.10 }, /* See ticket #1172 */
-  { path: "text-valign-TOP.pde", tags: ["2D", "Text"] },
-  { path: "text-valign-BOTTOM-m.pde", tags: ["2D", "Text"] },
+  { path: "text-valign-BOTTOM.pde", tags: ["2D", "Text"], epsilonOverride: 0.11 }, /* See ticket #1172 */
+  { path: "text-valign-BOTTOM-m.pde", tags: ["2D", "Text"], epsilonOverride: 0.11 }, /* See ticket #1172 */
   { path: "text-valign-BASELINE-m.pde", tags: ["2D", "Text"], epsilonOverride: 0.09 }, /* See ticket #1172 */
-  { path: "text-valign-CENTER-m.pde", tags: ["2D", "Text"], epsilonOverride: 0.06 }, /* See ticket #1172 */
-  { path: "text-valign-TOP-m.pde", tags: ["2D", "Text"], epsilonOverride: 0.06 }, /* See ticket #1172 */
+  { path: "text-valign-BASELINE.pde", tags: ["2D", "Text"], epsilonOverride: 0.09 }, /* See ticket #1172 */
+  { path: "text-valign-CENTER.pde", tags: ["2D", "Text"], epsilonOverride: 0.14 }, /* See ticket #1172 */
+  { path: "text-valign-CENTER-m.pde", tags: ["2D", "Text"], epsilonOverride: 0.09 }, /* See ticket #1172 */
+  { path: "text-valign-TOP.pde", tags: ["2D", "Text"], epsilonOverride: 0.10 }, /* See ticket #1172 */
+  { path: "text-valign-TOP-m.pde", tags: ["2D", "Text"], epsilonOverride: 0.10 }, /* See ticket #1172 */
   { path: "letters.pde", tags: ["2D", "Text"], epsilonOverride: 0.45 }, /* See ticket #1172 */
   { path: "words.pde", tags: ["2D", "Text"], epsilonOverride: 0.25 }, /* See ticket #1172 */
   { path: "simple.pde", tags: ["2D"] },
