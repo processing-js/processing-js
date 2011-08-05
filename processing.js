6ab8b211dcd98ab9a8f51fe7a0f9e6987d967cd5
Pomax <Pomax@nihongoresources.com>
2011-08-04 20:08:46 -0700

    added recaching of ascent/descent/leading when changing text size

--- processing.js
+++ processing.js
@@ -15757,6 +15757,10 @@
       if (size !== curTextSize) {
         curTextFont = PFont.get(curFontName, size);
         curTextSize = size;
+        // recache metrics
+        curTextAscent = curTextFont.ascent;
+        curTextDescent = curTextFont.descent;
+        curTextLeading = curTextFont.leading;
         var curContext = drawing.$ensureContext();
         curContext.font = curTextFont.css;
       }
