f5a9669d13f769734a8c2b5e02c04c00c326eb75
James Boelen <athrus@boelen.ca>
2011-09-29 02:21:34 -0400

    Close Bug 1417 Solution B to bug 1417 +If doFill is set to false, dont call p.fill in the sketch setup

--- processing.js
+++ processing.js
@@ -10123,7 +10123,8 @@
     */
     DrawingShared.prototype.size = function(aWidth, aHeight, aMode) {
       p.stroke(0);
-      p.fill(255);
+      if(doFill)
+	    p.fill(255);
 
       // The default 2d context has already been created in the p.init() stage if
       // a 3d context was not specified. This is so that a 2d context will be
